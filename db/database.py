3f23740d72e6a2933aab5c8004c0b6349ca40541
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-08-09 15:38:19 +0700

    feat: load DB name from env

--- db/database.py
+++ db/database.py
@@ -1,6 +1,12 @@
 import sqlite3
+from dotenv import load_dotenv # type: ignore
+import os
+
+load_dotenv()
+
+DB = os.getenv('DB')
 
 def get_db_connection():
-    conn = sqlite3.connect('hentek_blast.db')
+    conn = sqlite3.connect(DB)
     conn.row_factory = sqlite3.Row
     return conn
